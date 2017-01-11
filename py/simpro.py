ab189b3188d99bb58a1df15caf80187f273c35d8
NiL <csningli@gmailc.om>
2017-01-11 21:57:35 +0800

    Allow recursive components

--- py/simpro.py
+++ py/simpro.py
@@ -75,16 +75,17 @@ class SimpleProject :
                 and not os.path.isdir(os.path.join(path, name)) \
                 and not os.path.exists(os.path.join(path, name)) :
                 
-            self.log('Create project directory and pro_file.') 
+            self.log('Create project directory and profile.') 
             os.mkdir(os.path.join(path, name))
             profile = os.path.join(path, name, name + self.config['profile_ext'])
                     
-            # initialize and load the profile
+            # initialize the profile
             
             with open(profile, 'w') as f :
                 json.dump(self.context, f)
-                
-            self.load(profile)
+            
+            self.name = name
+            self.path = path
         else :
             self.error('Invalid \'name\' : %s, or \'path\' : %s' % (name, path)) 
             return False
@@ -92,10 +93,13 @@ class SimpleProject :
         return True
         
         
-    def load(self, profile) :
+    def load(self, profile = None) :
 
         # check if pro_file correctly indicates a Dao project.
         
+        if profile is None : 
+            profile = os.path.join(self.path, self.name, self.name + self.config['profile_ext'])
+            
         self.log('Load project with \'profile\' : %s' % profile)
         if profile.__class__.__name__ == 'str' \
                 and len(profile) > len(self.config['profile_ext']) \
@@ -117,11 +121,13 @@ class SimpleProject :
             if len(path) > 0 and len(name) > 0 and is_name_consistent :
                 self.path = path
                 self.name = name
-                
-                with open(profile, 'r') as f :
-                    self.context = json.load(f)
-                    
-                self.validate() 
+               
+                if self.validate() :
+                    with open(profile, 'r') as f :
+                        self.context = json.load(f)
+                else :
+                    self.error('Project is not valid.')
+                    return False 
             else :
                 self.error('Invalid \'profile\' : %s' % profile)
                 return False 
@@ -132,6 +138,16 @@ class SimpleProject :
         return True
 
 
+    def validate_component(self, component, entry, entry_path) :
+        component_path = os.path.join(self.path, self.name, entry_path, component)
+        if not os.path.isdir(component_path) :
+            os.mkdir(component_path)
+        if entry.__class__.__name__ == 'dict' :
+            for subcomponent in entry :
+                self.validate_component(subcomponent, entry[subcomponent], 
+                        os.path.join(entry_path, component)) 
+     
+        
     def validate(self) : # check if config and files are valid and consistent.
 
         self.log("Validate the project.")
@@ -145,62 +161,82 @@ class SimpleProject :
             if not os.path.exists(profile) :
                 self.error('The project has no valid profile: %s' % profile) 
                 return False
-                
+            
             for component in self.components.keys() :
-                component_path = os.path.join(self.path, self.name, component)
-                        
-                if not os.path.isdir(component_path) :
-                    os.mkdir(component_path)
+                self.validate_component(component, self.components[component], '') 
+                       
         except Exception as e:
             self.error("Exception happens during validating. %s" % e)
             return False
                         
         return True
     
-    
+    def component_new(self, component_seq, container, container_path, name, ext = '') :
+        component_seqsplit = component_seq.split('/')
+        if len(component_seqsplit) == 1 :
+            component = component_seqsplit[0]
+            if component in container.keys() :
+                if len(name) <= len(ext) or name[-len(ext) : ] != ext :
+                    name += ext
+                    
+                new_file = os.path.join(self.path, self.name, container_path, component, name)
+                        
+                if not os.path.exists(new_file) :
+                    with open(new_file, 'w') as f :
+                        f.write(container[component])
+                    self.log('Created file : %s' % new_file)
+                    
+                    return True
+                else :  
+                    self.error('Failed to create the file : %s. It already exists.' % new_file)
+                    return False
+            else :
+                self.error('Invalid \'component\' : %s' % component)
+                return False
+        elif len(component_seqsplit) > 1 and len(component_seqsplit[0]) > 0 :
+            parent = component_seqsplit[0]
+            if parent in container.keys() and container[parent].__class__.__name__ == 'dict' :
+                return self.component_new(
+                                os.path.join(*component_seqsplit[1:]), 
+                                container[parent],
+                                os.path.join(container_path, parent), 
+                                name, 
+                                ext
+                        )
+            else :
+                self.error('Invalid \'component\' : %s under \'container_path\'' % 
+                        (parent, container_path))
+                return False
+        else :
+            self.error('Invalid \'component_seq\' : %s' % component_seq)
+            return False
+
+
+
     def new(self, component, name, ext = '') :
     
         self.log("Create new file with \'component\' : %s, \'name\' : %s, \'ext\' : %s" % 
                 (component, name, ext))
-                
-        if component in self.components.keys() :
         
-            if len(name) <= len(ext) or name[-len(ext) : ] != ext :
-                name += ext
-                
-            new_file = os.path.join(self.path, self.name, component, name)
-                    
-            if not os.path.exists(new_file) :
-                with open(new_file, 'w') as f :
-                    f.write(self.components[component])
-                self.log('Created file : %s' % new_file)
-            else :  
-                self.error('Failed to create the file : %s. It already exists.' % new_file)
-                return False
-        else :
-            self.error('Invalid \'component\' : %s' % component)
-            return False
-
-        return True
+        return self.component_new(component, self.components, '',name, ext)
 
 
     def save(self) :
-        if self.validate() :
-            self.log("Project is valid.")
-            profile = os.path.join(self.path, self.name, self.name + self.config['profile_ext'])
-            
+        profile = os.path.join(self.path, self.name, self.name + self.config['profile_ext'])
+        if os.path.exists(profile) :
             with open(profile, 'w') as f :
                 json.dump(self.context, f)
+                
             self.log("Save the project.")
             
             return True
         else :
-            self.error("Project is not valid.")
+            self.error("Missing \'profile\': %s" % profile)
             return False
         
         
     def exec(self) :
-        self.save()
+        pass
 
     def print_logs(self) :
         print('\nLogs : \n' + '-' * 64)
